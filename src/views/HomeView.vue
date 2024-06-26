c5fafb02d0976b26df81d2059cfe14e88f2311a7
David FYON <davidfyon@gmail.com>
2024-06-26 15:31:18 +0200

    add stores

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,7 @@
   <v-row justify="center">
     <v-col
       cols="12"
-      md="6"
+      md="4"
     >
       <v-card
         color="grey-lighten-4"
@@ -24,7 +24,7 @@
     </v-col>
     <v-col
       cols="12"
-      md="6"
+      md="4"
     >
       <v-card
         color="grey-lighten-4"
@@ -42,13 +42,58 @@
         </v-card-item>
       </v-card>
     </v-col>
+    <v-col
+      cols="12"
+      md="4"
+    >
+      <v-card
+        color="grey-lighten-4"
+        class="mx-auto"
+      >
+        <v-card-title>
+          Test des stores
+        </v-card-title>
+        <v-card-item>
+          <div>Compteur: {{ counterStore?.count }}</div>
+          <div class="d-flex flex-wrap align-center justify-center">
+            <v-btn color="primary" class="ma-2" @click="counterStore?.increment">Incrémenter</v-btn>
+            <v-btn color="primary" class="ma-2" @click="counterStore?.decrement">Décrémenter</v-btn>
+            <v-btn color="primary" class="ma-2" @click="counterStore?.reset">Réinitialiser</v-btn>
+          </div>
+        </v-card-item>
+        <v-card-item>
+          <div>Notification: {{ notificationStore?.notificationPayload }}</div>
+          <div class="d-flex flex-wrap align-center justify-center">
+            <v-btn color="primary" class="ma-2" @click="createNotification">Créer une notification</v-btn>
+            <v-btn color="primary" class="ma-2" @click="removeNotification">Supprimer une notification</v-btn>
+          </div>
+        </v-card-item>
+      </v-card>
+    </v-col>
   </v-row>
 </template>
 
 <script setup lang="ts">
 import HelloWorld from '../components/HelloWorld.vue';
 import { BackBtn } from '@cnamts/synapse-bridge';
+
 import { getConfig } from '../composables/injectEnv';
+import { useCounterStore } from "../stores/counter";
+import { useNotificationStore } from "../stores/notifications";
 
 const data = getConfig();
+const counterStore = useCounterStore();
+const notificationStore = useNotificationStore();
+
+function createNotification() {
+  const payload = {
+    message: 'Notification de test',
+    type: 'success'
+  };
+  notificationStore.create(payload);
+}
+
+function removeNotification() {
+  notificationStore.remove();
+}
 </script>
